c9dbe5da2282162a5432bebbc2eaaf5986c886da
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-10-10 15:50:00 +0900

    DispatcherServlet - Customizing special type bean (ex) ViewResolver)

--- javaservletdemo/src/main/java/com/sboo/WebConfig.java
+++ javaservletdemo/src/main/java/com/sboo/WebConfig.java
@@ -1,9 +1,19 @@
 package com.sboo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan
 public class WebConfig {
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 }
