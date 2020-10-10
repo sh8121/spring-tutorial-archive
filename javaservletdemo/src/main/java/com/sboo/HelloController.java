4e54e2c421c29ffaad4f76bec9a86dfd4c603003
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-10-10 15:13:22 +0900

    DispatcherServlet - various way to handle Request

--- javaservletdemo/src/main/java/com/sboo/HelloController.java
+++ javaservletdemo/src/main/java/com/sboo/HelloController.java
@@ -1,16 +1,24 @@
 package com.sboo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
     @Autowired
     private HelloService helloService;
 
     @GetMapping("/hello")
+    @ResponseBody
     public String hello() {
         return "Hello, " + helloService.getName();
+    }
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "/WEB-INF/sample.jsp";
     }
 }
