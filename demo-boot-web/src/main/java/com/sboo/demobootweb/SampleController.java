88eee306c1145d62511dc21d95f45b159e26172f
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-10-11 15:57:37 +0900

    SpringBoot SpringMVC - DomainClassConverter

--- demo-boot-web/src/main/java/com/sboo/demobootweb/SampleController.java
+++ demo-boot-web/src/main/java/com/sboo/demobootweb/SampleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
     @GetMapping("/hello")
-    public String hello(@RequestParam("name") Person person) {
+    public String hello(@RequestParam("id") Person person) {
         return "hello, " + person.getName();
     }
 }
--- demo-boot-web/src/test/java/com/sboo/demobootweb/SampleControllerTest.java
+++ demo-boot-web/src/test/java/com/sboo/demobootweb/SampleControllerTest.java
@@ -16,10 +16,17 @@ class SampleControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    PersonRepository personRepository;
+
     @Test
     public void hello() throws Exception {
+        Person person = new Person();
+        person.setName("sboo");
+        Person savedPerson = personRepository.save(person);
+
         mockMvc.perform(get("/hello")
-                .param("name", "sboo"))
+                .param("id", savedPerson.getId().toString()))
                 .andDo(print())
                 .andExpect(content().string("hello, sboo"));
     }
