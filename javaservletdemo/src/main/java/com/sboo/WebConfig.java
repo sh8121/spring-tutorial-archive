219c4b7bde2de5797c4ddbaffb0d149612c9cef4
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-10-11 11:00:36 +0900

    Spring MVC Config Customization by implementing WebMvcConfigurer

--- javaservletdemo/src/main/java/com/sboo/WebConfig.java
+++ javaservletdemo/src/main/java/com/sboo/WebConfig.java
@@ -5,17 +5,16 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
-public class WebConfig {
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/", ".jsp");
     }
 }
