df985cd73c169a83c787edbcef28d5413344dcfb
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-10-11 20:42:58 +0900

    SpringBoot SpringMVC - HandlerInterceptor

--- demo-boot-web/src/main/java/com/sboo/demobootweb/SampleController.java
+++ demo-boot-web/src/main/java/com/sboo/demobootweb/SampleController.java
@@ -6,8 +6,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
+    // preHandle 1
+    // preHandle 2
+    // 요청 처리
+    // postHandler 2
+    // postHandler 1
+    // 뷰 랜더링
+    // afterCompletion 2
+    // afterCompletion 1
+
     @GetMapping("/hello")
     public String hello(@RequestParam("id") Person person) {
         return "hello, " + person.getName();
+    }
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "hello";
+    }
+
+    @GetMapping("/h1")
+    public String h1() {
+        return "h1";
     }
 }
--- demo-boot-web/src/main/java/com/sboo/demobootweb/WebConfig.java
+++ demo-boot-web/src/main/java/com/sboo/demobootweb/WebConfig.java
@@ -1,8 +1,16 @@
 package com.sboo.demobootweb;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new GreetingInterceptor()).order(0);
+        registry.addInterceptor(new AnotherInterceptor())
+                .addPathPatterns("/h1")
+                .order(-1);
+    }
 }
