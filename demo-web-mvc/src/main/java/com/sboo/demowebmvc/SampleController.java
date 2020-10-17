e368e26086db5789b174bc18ba53d3ad1a13c2e9
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-10-17 19:58:47 +0900

    SpringMVC - Mapping(HttpMethod)

---
+++ demo-web-mvc/src/main/java/com/sboo/demowebmvc/SampleController.java
@@ -0,0 +1,16 @@
+package com.sboo.demowebmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(method = RequestMethod.GET)
+public class SampleController {
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ demo-web-mvc/src/test/java/com/sboo/demowebmvc/SampleControllerTest.java
@@ -0,0 +1,32 @@
+package com.sboo.demowebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+class SampleControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void helloTest() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        mockMvc.perform(put("/hello"))
+                .andDo(print())
+                .andExpect(status().isMethodNotAllowed());
+
+        mockMvc.perform(post("/hello"))
+                .andDo(print())
+                .andExpect(status().isMethodNotAllowed());
+    }
+}
