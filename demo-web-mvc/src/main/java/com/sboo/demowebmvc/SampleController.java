78e6b06dfcee7959ab41bed96a6dcb2900d2b7cc
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-10-18 21:00:16 +0900

    SpringMVC - Handler(@RequestParam with Map)

--- demo-web-mvc/src/main/java/com/sboo/demowebmvc/SampleController.java
+++ demo-web-mvc/src/main/java/com/sboo/demowebmvc/SampleController.java
@@ -4,16 +4,18 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @Controller
 public class SampleController {
 
     @PostMapping("/events")
     @ResponseBody
-    public Event getEvent(@RequestParam String name,
-                          @RequestParam Integer limit) {
+    public Event getEvent(@RequestParam Map paramMap) {
+        //Map or MultiValueMap
         Event event = new Event();
-        event.setName(name);
-        event.setLimit(limit);
+        event.setName((String)paramMap.get("name"));
+        event.setLimit(Integer.parseInt((String)paramMap.get("limit")));
         return event;
     }
 }
