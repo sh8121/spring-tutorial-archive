7e3e582504aae2dd509d0ddfcbdaf65c24567068
Boo, Sang Hun (부상훈) <sboo@ebay.com>
2020-10-18 21:46:43 +0900

    SpringMVC - Handler(Thymeleaf Form)

--- demo-web-mvc/src/main/java/com/sboo/demowebmvc/SampleController.java
+++ demo-web-mvc/src/main/java/com/sboo/demowebmvc/SampleController.java
@@ -2,12 +2,21 @@ package com.sboo.demowebmvc;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Controller
 public class SampleController {
+
+    @GetMapping("/events/form")
+    public String eventsForm(Model model) {
+        Event newEvent = new Event();
+        newEvent.setLimit(50);
+        model.addAttribute("event", newEvent);
+        return "/events/form";
+    }
 
     @PostMapping("/events")
     @ResponseBody
